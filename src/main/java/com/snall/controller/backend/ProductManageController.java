3b1ffac235fec50f8ec542afea01ae6234aa6d47
momoka <454768497@qq.com>
2018-09-10 15:06:44 +0800

    springmvc文件上传

--- src/main/java/com/snall/controller/backend/ProductManageController.java
+++ src/main/java/com/snall/controller/backend/ProductManageController.java
@@ -1,19 +1,27 @@
 package com.snall.controller.backend;
 
+import com.google.common.collect.Maps;
 import com.snall.common.Const;
 import com.snall.common.ResponseCode;
 import com.snall.common.ServerResponse;
 import com.snall.pojo.Product;
 import com.snall.pojo.User;
+import com.snall.service.IFileService;
 import com.snall.service.IProductService;
 import com.snall.service.IUserService;
+import com.snall.util.PropertiesUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/manage/product")
@@ -23,6 +31,8 @@ public class ProductManageController {
     private IUserService iUserService;
     @Autowired
     private IProductService iProductService;
+    @Autowired
+    private IFileService iFileService;
 
 
     //ServerResponse 标准的code msg T data
@@ -116,6 +126,72 @@ public class ProductManageController {
             return ServerResponse.createByErrorMessage("无权限操作");
         }
     }
+    @RequestMapping("upload.do")
+    @ResponseBody
+    public ServerResponse upload(HttpSession session,@RequestParam(value = "upload_file",required = false) MultipartFile file, HttpServletRequest request){
+        //session检测登录
+        User user= (User) session.getAttribute(Const.CURRENT_USER);
+        //如果未登录
+        if (user==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，请登录管理员");
+        }
+        //判断用户是否拥有权限 数字确定权限，这里是简单的管理员用户区分
+        if (iUserService.checkAdminRole(user).isSuccess()){
+            //填充我们增加产品的业务逻辑
+            String path=request.getSession().getServletContext().getRealPath("upload");
+            String targetFileName=iFileService.upload(file,path);
+            String url= PropertiesUtil.getProperty("ftp.server.http.prefix")+targetFileName;
+
+            Map fileMap= Maps.newHashMap();
+            fileMap.put("uri",targetFileName);
+            fileMap.put("url",url);
+
+            return ServerResponse.createBySuccess(fileMap);
+        }else {
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+
+    }
+
+    @RequestMapping("richtext_img_upload.do")
+    @ResponseBody
+    public Map richtextImgUpload(HttpSession session, @RequestParam(value = "upload_file",required = false) MultipartFile file, HttpServletRequest request, HttpServletResponse response){
+        Map resultMap=Maps.newHashMap();
+        //session检测登录
+        User user= (User) session.getAttribute(Const.CURRENT_USER);
+        //如果未登录
+        if (user==null){
+            resultMap.put("success",false);
+            resultMap.put("msg","请登录管理员");
+            return resultMap;
+        }
+        //判断用户是否拥有权限 数字确定权限，这里是简单的管理员用户区分
+        //富文本对返回值有自己的要求
+        if (iUserService.checkAdminRole(user).isSuccess()){
+            //填充我们增加产品的业务逻辑
+            String path=request.getSession().getServletContext().getRealPath("upload");
+            String targetFileName=iFileService.upload(file,path);
+            if (StringUtils.isBlank(targetFileName)){
+                resultMap.put("success",false);
+                resultMap.put("msg","上传失败");
+                return resultMap;
+            }
+            String url= PropertiesUtil.getProperty("ftp.server.http.prefix")+targetFileName;
+            resultMap.put("success",true);
+            resultMap.put("msg","上传成功");
+            resultMap.put("file_path",url);
+
+            response.addHeader("Access-Control-Allow-Headers","X-File-Name");
+            return resultMap;
+        }else {
+            resultMap.put("success",false);
+            resultMap.put("msg","无权限操作");
+            return resultMap;
+        }
+
+    }
+
+
 
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,20 @@
+<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
 <html>
 <body>
 <h2>Hello World123!</h2>
+
+springmvc上传文件
+<form name="form1" action="/manage/product/upload.do" method="post" enctype="multipart/form-data">
+    <input type="file" name="upload_file"/>
+    <input type="submit" value="springmvc上传文件"/>
+
+</form>
+
+springmvc上传文件富文本
+<form name="form1" action="/manage/product/richtext_img_upload.do" method="post" enctype="multipart/form-data">
+    <input type="file" name="upload_file"/>
+    <input type="submit" value="富文本文件上传"/>
+
+</form>
 </body>
 </html>
